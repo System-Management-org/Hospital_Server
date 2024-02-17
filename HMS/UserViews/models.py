3a41929144902aee843a5ce507ef6b39132fa799
KAsare1 <asarek977@gmail.com>
2024-02-17 04:36:45 +0000

    d

--- HMS/UserViews/models.py
+++ HMS/UserViews/models.py
@@ -65,7 +65,7 @@ class Patient(models.Model):
     email = models.EmailField(max_length=200, null=True, blank=True)
     phone = models.CharField(max_length=200, null=True, blank=True)
     res_address = models.CharField(max_length=200)
-    registrar_id = models.OneToOneField(User, on_delete=models.CASCADE, null=True,blank=True, unique=False)#make foreign key once we get staff model
+    registrar = models.OneToOneField(User, on_delete=models.CASCADE, null=True,blank=True, unique=False)#make foreign key once we get staff model
 
     def __str__(self):
         return self.first_name + " " + self.last_name
