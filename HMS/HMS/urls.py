b27ae375002e89f9248d2d307afcd1931650026a
KAsare1 <asarek977@gmail.com>
2024-02-17 03:30:33 +0000

    dd

--- HMS/HMS/urls.py
+++ HMS/HMS/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('UserViews.urls')),
 ]
