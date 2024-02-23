c68e8c7730f651548f100868c1443050168195a8
KAsare1 <asarek977@gmail.com>
2024-02-23 17:27:45 +0000

    d

--- HMS/UserViews/models.py
+++ HMS/UserViews/models.py
@@ -15,18 +15,6 @@ class Doctor(models.Model):
     def __str__(self):
         return self.first_name + " " + self.last_name
     
-class Test(models.Model):
-    staff_id = models.CharField(max_length=200)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=50)
-    birthdate = models.DateField()
-    email = models.EmailField(max_length=200)
-    phone = models.CharField(max_length=200)
-    speciality = models.CharField(max_length=200)
-   
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name
     
 class Nurse(models.Model):
     staff_id = models.CharField(max_length=200)
