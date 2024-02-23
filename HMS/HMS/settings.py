df1d190f5b8f92cc7df39faadd683290346bfea7
KAsare1 <asarek977@gmail.com>
2024-02-23 16:42:50 +0000

    d

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -88,17 +88,17 @@ WSGI_APPLICATION = 'HMS.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'spmsug$HMS',
-#         'USER': 'spmsug',
-#         'PASSWORD': '.7:qBhM28AF-pzP',
-#         'HOST': 'spmsug.mysql.pythonanywhere-services.com',
-# }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spmsug$HMS',
+        'USER': 'spmsug',
+        'PASSWORD': '.7:qBhM28AF-pzP',
+        'HOST': 'spmsug.mysql.pythonanywhere-services.com',
+}
 }
 
 
