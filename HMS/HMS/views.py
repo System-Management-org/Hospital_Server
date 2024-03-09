9ac5d443b378618c298ea3f4e76d12d4de8387bd
KAsare1 <asarek977@gmail.com>
2024-03-09 10:29:04 +0000

    ss

--- HMS/HMS/views.py
+++ HMS/HMS/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-from HMS.HMS.serializers import UserSerializer
+from .serializers import UserSerializer
 
 class MyApiView(APIView):
     permission_classes = [IsAuthenticated]
